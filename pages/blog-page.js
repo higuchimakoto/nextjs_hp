55cb4949684d38727b337b257dfc5806dde42ee9
higuchimakoto <makoto-higuchi@tapp-co.jp>
2021-09-14 21:14:06 +0900

    add:post投稿プレフェッチ機能

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,7 +1,24 @@
 import Layout from '../compents/Layout'
+import { getAllPostsData } from '../ lib/post'
+import Post from '../compents/Post'
 
-const Blog = () => {
-	return <Layout title="Blog">Blog page</Layout>
+const Blog = ({ posts }) => {
+	return (
+		<Layout title="Blog">
+			<ul className="m-10">
+				{posts &&
+					posts.map((post) => <Post key={post.id}  post={post} />
+					)}
+			</ul>
+		</Layout>
+	)
 }
 
 export default Blog
+
+export const getStaticProps = async () => {
+	const posts = await getAllPostsData()
+	return {
+		props: { posts },
+	}
+}
